3ada2b2d1ca4254d4bf8b73259257acb3da69fce
josepaltadill <josepaltadill@hotmail.com>
2022-09-05 13:33:21 +0200

    implementando aplicacion de notas - datos iniciales + gennerador de ids + borrador para el boton añadir

---
+++ app/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Notes from './Notes';
+
+export default() => (
+    <div>
+        <button onClick={() => console.log('añadir nota')}>+</button>
+        <Notes />
+    </div>
+);
