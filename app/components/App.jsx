48cd218058950b639360f7cd79ac98d589ef1931
josepaltadill <josepaltadill@hotmail.com>
2022-09-06 20:47:41 +0200

    añadida funcion añadir tareas (siempre la misma)

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 import Notes from './Notes';
+import uuid from 'uuid';
 
-export default() => (
-    <div>
-        <button onClick={() => console.log('añadir nota')}>+</button>
-        <Notes />
-    </div>
-);
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                },
+                {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        }
+    }
+    render() {
+        const {notes} = this.state;
+
+        return (
+            <div>
+                <button onClick={this.addNote}>+</button>
+                <Notes notes={notes} />
+            </div>
+        );
+    }
+
+    addNote = () => {
+        this.setState({
+            notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: 'New task'
+            }])
+        });
+    }
+}
