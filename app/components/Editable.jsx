afa05244f6def00c8e3c36ecd9149a5143c019a8
josepaltadill <josepaltadill@hotmail.com>
2022-09-11 18:55:06 +0200

    Haciendo un seguimiento del estado - Implementación de edit

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -8,8 +8,29 @@ export default ({editing, value, onEdit, ...props}) => {
     return <span {...props}>{value}</span>
 }
 
-const Edit = ({onEdit = () => {}, value, ...props}) => (
-    <div onClick={onEdit} {...props}>
-        <span>edit: {value}</span>
-    </div>
-);
+class Edit extends React.Component {
+    render() {
+        const {value, onEdit, ...props} = this.props;
+
+        return <input 
+            type="text"
+            autoFocus={true}
+            defaultValue={value}
+            onBlur={this.finishEdit}
+            onKeyPress={this.checkEnter}
+            {...props}
+        />
+    }
+    checkEnter = (e) => {
+        if(e.key === 'Enter') {
+            this.finishEdit(e);
+        }
+    }
+    finishEdit = (e) => {
+        const value = e.target.value;
+
+        if(this.props.onEdit) {
+            this.props.onEdit(value);
+        }
+    }
+}
