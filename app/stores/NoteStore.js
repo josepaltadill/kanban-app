b8b02f475c26d17bc08b501e3bc143e5dafea4cb
josepaltadill <josepaltadill@hotmail.com>
2022-10-04 19:37:30 +0200

    implementando NoteStore y NoteActions

---
+++ app/stores/NoteStore.js
@@ -0,0 +1,41 @@
+import uuid from 'uuid';
+import NoteActions from '../actions/NoteActions';
+
+export default class NoteStore {
+    constructor() {
+        this.bindActions(NoteActions);
+
+        this.notes = [
+            {
+                id: uuid.v4(),
+                task: 'Aprender React'
+            },
+            {
+                id: uuid.v4(),
+                task: 'Hacer la colada'
+            }
+        ];
+    }
+
+    create(note) {
+        this.setState({
+            notes: this.notes.concat(note)
+        });
+    }
+    update(updatedNote) {
+        this.setState({
+            notes : this.notes.map(note => {
+                if(note.id === updatedNote.id) {
+                    return Object.assign({}, note, updatedNote);
+                }
+
+                return note;
+            })
+        });
+    }
+    delete(id) {
+        this.setState({
+            notes: this.notes.filter(note => note.id !== id)
+        });
+    }
+}
